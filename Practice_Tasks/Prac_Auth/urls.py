ce82c662b4b787c4a2201f19f1173fdf21247023
Tanishqua-Simform <tanishqua.bansal@simformsolutions.com>
2025-03-13 19:56:18 +0530

    Integrated decorators and Change password feature!

--- Practice_Tasks/Prac_Auth/urls.py
+++ Practice_Tasks/Prac_Auth/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import PasswordChangeView
-from .views import register, login_form, home, dashboard, logout_form
+from .views import register, login_form, home, dashboard, logout_form, profile
 
 urlpatterns = [
-    path('register', register, name="register"),
-    path('login', login_form, name="auth-login"),
-    path('logout', logout_form, name="auth-logout"),
-    path('change-password', PasswordChangeView.as_view(template_name='Prac_Auth/password-change.html'), name="auth-login"),
     path('home', home, name="home"),
-    path('dashboard', dashboard, name="auth-dashboard"),
+    path('register', register, name="register"),
+    path('login', login_form, name="auth_login"),
+    path('change-password', PasswordChangeView.as_view(template_name='Prac_Auth/password-change.html', success_url='dashboard'), name="change_password"),
+    path('dashboard', dashboard, name="auth_dashboard"),
+    path('profile', profile, name="auth_profile"),
+    path('logout', logout_form, name="auth_logout"),
 ]
--- Practice_Tasks/Prac_Auth/views.py
+++ Practice_Tasks/Prac_Auth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import RegisterForm, LoginForm
 
 def home(request):
@@ -14,7 +16,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('auth-login')
+            return redirect('auth_login')
     else:
         form = RegisterForm()
     return render(request, 'Prac_Auth/register.html', {'form': form})
@@ -26,10 +28,16 @@ def login_form(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('auth-dashboard')
+            return redirect('auth_dashboard')
     form = LoginForm()
     return render(request, 'Prac_Auth/login.html', {'form': form})
 
 def logout_form(request):
     logout(request)
     return redirect('home')
+
+@login_required(login_url="auth_login")
+def profile(request):
+    user = User.objects.get(username=request.user)
+    html = f'<h1>Profile:</h1><h3>{user.username} ({user.email})</h3>'
+    return HttpResponse(html)
